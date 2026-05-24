af3deb92d538077c6d772a57751a0b32cb6df22d
Krisna Anggara <ptkrisnaanggara@gmail.com>
2026-05-24 13:35:03 +0800

    Enable admin payment receive endpoint

--- apps/api/src/modules/admin/admin.module.ts
+++ apps/api/src/modules/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AdminApiKeyGuard } from '../auth/admin-api-key.guard';
+import { PaymentAdminController } from '../payment/payment-admin.controller';
+import { PaymentWebhookService } from '../payment/payment-webhook.service';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
-  controllers: [AdminController],
-  providers: [AdminService, AdminApiKeyGuard],
+  controllers: [AdminController, PaymentAdminController],
+  providers: [AdminService, AdminApiKeyGuard, PaymentWebhookService],
 })
 export class AdminModule {}
