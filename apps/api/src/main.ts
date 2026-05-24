b64f32af43dea0359d419731f260e7f4f55f9483
Krisna Anggara <ptkrisnaanggara@gmail.com>
2026-05-24 13:13:02 +0800

    Add API bootstrap

---
+++ apps/api/src/main.ts
@@ -0,0 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  app.enableCors({ origin: true, credentials: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
+  await app.listen(config.get<number>('API_PORT', 3000));
+}
+
+bootstrap();
