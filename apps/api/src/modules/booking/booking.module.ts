a5c537ce0bc5cf31b4e05082c4a923e7763b3941
Krisna Anggara <ptkrisnaanggara@gmail.com>
2026-05-24 13:32:47 +0800

    Register booking expiry feature

--- apps/api/src/modules/booking/booking.module.ts
+++ apps/api/src/modules/booking/booking.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BookingExpiryController } from './booking-expiry.controller';
+import { BookingExpiryService } from './booking-expiry.service';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 
 @Module({
-  controllers: [BookingController],
-  providers: [BookingService],
+  controllers: [BookingController, BookingExpiryController],
+  providers: [BookingService, BookingExpiryService],
 })
 export class BookingModule {}
